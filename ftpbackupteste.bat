cce44f6d8eba426cd5e3d36c8e2ad73c93c781c9
dacostapiece <101139745+dacostapiece@users.noreply.github.com>
2024-03-11 21:23:50 -0300

    Create ftpbackupteste.bat

---
+++ ftpbackupteste.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\Users\Administrador\AppData\Local\Programs\Python\Python312\python.exe" "C:\BackupFTP-PABX-Python\FTPBACKUP_02\FIX MODULAR IMPORT\ftpbackupteste.py"
+pause
+REM create batch file ftpbackupteste.bat and use task scheduler
+REM remember to change in bat file, where your python exe and python script is at
